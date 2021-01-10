5eb935ded2641adb2ba93a415a3af17cf1e7900b
David Stearns <daveajstearns@gmail.com>
2021-01-10 01:22:12 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,39 +1,39 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19) {
-    return console.log("You are a teenager!");
-  };
+    return "You are a teenager!";
+  }
 };
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    return console.log("You are a teenager!");
+    return "You are a teenager!";
   }
   else {
-    return console.log("You are not a teenager");
+    return "You are not a teenager";
   }
 };
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
-    return console.log("You are a teenager!");
+    return "You are a teenager!";
   }
-  elseif (age < 13) {
-    return console.log("You are a kid.");
+  else if (age < 13) {
+    return "You are a kid.";
   }
-  elseif (age > 19) {
-    return console.log("You are a grownup");
+  else if (age > 19) {
+    return "You are a grownup";
   }
 };
 
 function ternaryTeenager(age) {
-  (age >= 13 && age <= 19) ? console.log("You are a teenager") : console.log("You are not a teenager")
+  (age >= 13 && age <= 19) ? return "You are a teenager" : return "You are not a teenager"
 };
 
 function switchAge(age) {
   switch (age) {
     case (age >= 13 && age <= 19):
-      return console.log("You are a teenager")
+      return "You are a teenager"
     default:
-      return console.log("You have an age")
+      return "You have an age"
   }
 };
